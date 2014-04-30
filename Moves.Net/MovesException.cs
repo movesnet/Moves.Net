208714599c476ab03b40445ce00e9241ee378fa7
Arnoud de Boer <info@sterk-internet.nl>
2014-04-30 22:29:57 +0200

    Added the http status code to the exception

--- Moves.Net/MovesException.cs
+++ Moves.Net/MovesException.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using Moves.Net.Model;
 using Newtonsoft.Json;
 using RestSharp;
@@ -8,18 +9,26 @@ namespace Moves.Net
 	[Serializable]
 	public class MovesException : Exception
 	{
-		public static MovesException FromErrorResponse(IRestResponse response) {
-			var error = JsonConvert.DeserializeObject<Error>(response.Content);
-
-			return new MovesException(error.Code);
-		}
+		public HttpStatusCode StatusCode { get; private set; }		
 
 		public MovesException() { }
+		
 		public MovesException(string message) : base(message) { }
+
+		public MovesException(string message, HttpStatusCode statusCode): base(message) {
+			this.StatusCode = statusCode;
+		}
+		
 		public MovesException(string message, Exception inner) : base(message, inner) { }
+		
 		protected MovesException(
 		  System.Runtime.Serialization.SerializationInfo info,
 		  System.Runtime.Serialization.StreamingContext context)
 			: base(info, context) { }
+
+		public static MovesException FromErrorResponse(IRestResponse response) {
+			var error = JsonConvert.DeserializeObject<Error>(response.Content);
+			return new MovesException(error.Code, response.StatusCode);
+		}
 	}
 }
