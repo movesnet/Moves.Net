d3efa3b3d8e098c642a50f68d48bce0cb5a78e07
Arnoud de Boer <info@sterk-internet.nl>
2014-05-05 14:02:15 +0200

    Bugfix: wrong assembly folder

--- nuget/build-package.cmd
+++ nuget/build-package.cmd
@@ -1,3 +1,3 @@
 path "c:\program files (x86)\nuget"
-copy "..\Moves.Net\bin\Release\Moves.Net.dll" "lib\net40\Moves.Net.dll"
+copy "..\Moves.Net\bin\Nuget\Moves.Net.dll" "lib\net40\Moves.Net.dll"
 nuget pack Package.nuspec
